abe9cb53131e32dca5b4add01872b03453366b44
enesxsec <120325383+ghost0x02@users.noreply.github.com>
2024-10-16 23:59:31 +0300

    Create categories.py

---
+++ categories.py
@@ -0,0 +1,26 @@
+from colorama import Fore, Style
+import time
+
+
+print(Fore.RED + """
+   _____ ____    __
+  / ___// __ \  / / 
+  \__ \/ / / / / /
+ ___/ / /_/ / / /___
+/____/\___\_\/_____/
+                    """)
+time.sleep(1)
+
+print("""
+ +--------------+
+| 1)categories  |
++--------------+""")
+
+print("LÜTFEN BİR SEÇENEK GİRİN")
+
+
+islemno = input("root@XEN-web:~ ")
+
+if islemno == "1":
+
+   print("""NOT FOUND""")
