eb7a2b5d270fc89f42c171e5e71ca0f6bbd3c882
enesxsec <120325383+ghost0x02@users.noreply.github.com>
2024-10-18 08:46:14 +0300

    Create images.py

---
+++ images.py
@@ -0,0 +1,226 @@
+from colorama import Fore, Style
+import time
+
+
+print(Fore.RED + """
+   _____ ____    __
+  / ___// __ \  / /
+  \__ \/ / / / / /
+ ___/ / /_/ / / /___
+/____/\___\_\/_____/
+                    """)
+time.sleep(1)
+
+print("""
+ +--------------+
+| 1)user         |
++--------------+""")
+
+print("LÜTFEN BİR SEÇENEK GİRİN")
+
+
+islemno = input("root@XEN-web:~ ")
+
+if islemno == "1":
+
+   print("""
++-------------+---------+
+| Column      | Type    |
++-------------+---------+
+| 1)category_id | int(11) |
+| 2)image       | text    |
+| 3)img_id      | int(11) |
++-------------+---------+""")
+
+print("LÜTFEN BİR SEÇENEK GİRİN")
+
+islemno = input("root@XEN-web:~ ")
+
+if islemno == "1":
+
+   print("""
++-------------+
+| category_id |
++-------------+
+| 5           |
+| 5           |
+| 1           |
+| 1           |
+| 1           |
+| 2           |
+| 2           |
+| 3           |
+| 3           |
+| 8           |
+| 8           |
+| 10          |
+| 10          |
+| 10          |
+| 13          |
+| 24          |
+| 24          |
+| 24          |
+| 24          |
+| 24          |
+| 24          |
+| 24          |
+| 24          |
+| 24          |
+| 24          |
+| 24          |
+| 24          |
+| 24          |
+| 24          |
+| 24          |
+| 24          |
+| 24          |
+| 24          |
+| 21          |
+| 21          |
+| 21          |
+| 13          |
+| 14          |
+| 14          |
+| 0           |
+| 0           |
+| 0           |
+| 14          |
+| 21          |
+| 15          |
+| 43          |
+| 13          |
+| 53          |
+| 43          |
+| 54          |
+| 54          |
+| 54          |
+| 0           |
+| 54          |
+| 43          |
++-------------+""")
+
+if islemno == "2":
+
+   print("""
++-------------------------------------------------------------+
+| image                                                       |
++-------------------------------------------------------------+
+| uploads/demoIMG_0930.JPG.jpg                                |
+| uploads/demoIMG_0933.JPG.jpg                                |
+| uploads/demoIMG_0933.JPG.jpg                                |
+| uploads/demoIMG_0894.JPG.jpg                                |
+| uploads/demo/IMG_0911.JPG.jpg                               |
+| uploads/demo/download (1).jpg.jpg                           |
+| uploads/demo/download.jpg.jpg                               |
+| uploads/demo/lahore.jpg.jpg                                 |
+| uploads/demo/pakistan.jpg.jpg                               |
+| uploads/demo/1416240987460.jpg.jpg                          |
+| uploads/demo/63c017b8d42e627dd704e48fe03e31ce.jpg.jpg       |
+| uploads/demo/6209-0_new-world-city-6209_l.jpg.jpg           |
+| uploads/demo/63c017b8d42e627dd704e48fe03e31ce.jpg.jpg       |
+| uploads/demo/____-__-_____-_-____-2015-____-_______.jpg.jpg |
+| uploads/demo/IMG-20170311-WA0006.jpg.jpg                    |
+| uploads/demo/HORIZON...............1.jpg.jpg                |
+| uploads/demo/HORIZON...............2.jpg.jpg                |
+| uploads/demo/HORIZON...............3.jpg.jpg                |
+| uploads/demo/HORIZON...............4.jpg.jpg                |
+| uploads/demo/HORIZON...............5.jpg.jpg                |
+| uploads/demo/HORIZON...............6.jpg.jpg                |
+| uploads/demo/HORIZON...............7.jpg.jpg                |
+| uploads/demo/HORIZON...............8.jpg.jpg                |
+| uploads/demo/HORIZON...............9.jpg.jpg                |
+| uploads/demo/HORIZON...............10.jpg.jpg               |
+| uploads/demo/HORIZON...............11.jpg.jpg               |
+| uploads/demo/HORIZON...............12.jpg.jpg               |
+| uploads/demo/HORIZON...............13.jpg.jpg               |
+| uploads/demo/HORIZON...............14.jpg.jpg               |
+| uploads/demo/HORIZON...............15.jpg.jpg               |
+| uploads/demo/HORIZON...............16.jpg.jpg               |
+| uploads/demo/HORIZON...............17.jpg.jpg               |
+| uploads/demo/HORIZON...............18.jpg.jpg               |
+| uploads/demo/IMG-20170123-WA0141.jpg.jpg                    |
+| uploads/demo/IMG-20170123-WA0158.jpg.jpg                    |
+| uploads/demo/IMG-20170131-WA0023.jpg.jpg                    |
+| uploads/demo/5 (2).jpg.jpg                                  |
+| uploads/demo/IMG-20170420-WA0012.jpg.jpg                    |
+| uploads/demo/IMG-20170420-WA0018.jpg.jpg                    |
+| uploads/demo/IMG-20170421-WA0003.jpg.jpg                    |
+| uploads/demo/IMG-20170421-WA0003.jpg.jpg                    |
+| uploads/demo/IMG-20170420-WA0038.jpg.jpg                    |
+| uploads/demo/Front Slider 1.jpg.jpg                         |
+| uploads/demo/IMG-20170426-WA0025.jpg.jpg                    |
+| uploads/demo/IMG-20170420-WA0026.jpg.jpg                    |
+| uploads/demo/Abdul.jpeg.jpeg                                |
+| uploads/demo/alfa - Copy.php.jpg.jpg                        |
+| uploads/demo/a.php.jpg.jpg                                  |
+| uploads/demo/a.php.jpg.jpg                                  |
+| uploads/demo/tf.png.png                                     |
+| uploads/demo/index.jpg.jpg                                  |
+| uploads/demo/images.jpg.jpg                                 |
+| uploads/demo/images.jpg.jpg                                 |
+| uploads/demo/images.jpg.jpg                                 |
+| uploads/demo/file.php.png.png                               |
++-------------------------------------------------------------+""")
+
+if islemno == "3":
+
+   print("""
++--------+
+| img_id |
++--------+
+| 11     |
+| 12     |
+| 13     |
+| 14     |
+| 18     |
+| 19     |
+| 20     |
+| 21     |
+| 22     |
+| 23     |
+| 24     |
+| 26     |
+| 27     |
+| 28     |
+| 44     |
+| 45     |
+| 46     |
+| 47     |
+| 48     |
+| 49     |
+| 50     |
+| 51     |
+| 52     |
+| 53     |
+| 54     |
+| 55     |
+| 56     |
+| 57     |
+| 58     |
+| 59     |
+| 60     |
+| 61     |
+| 62     |
+| 63     |
+| 64     |
+| 65     |
+| 66     |
+| 67     |
+| 68     |
+| 69     |
+| 70     |
+| 71     |
+| 72     |
+| 73     |
+| 74     |
+| 93     |
+| 94     |
+| 95     |
+| 96     |
+| 103    |
+| 106    |
+| 109    |
+| 110    |
+| 111    |
+| 113    |
++--------+""")
